a85b209b81020c405ccc7f3d49863f978ba6c1eb
mpenacasares <m.penacasares@gmail.com>
2022-01-18 08:50:29 +0100

    Add template react starter kit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import "../styles/App.scss";
+
+function App() {
+  return (
+    <div>
+      {/* <p>Mi código comentado</p> */}
+      <h1>Hola mundo</h1>
+    </div>
+  );
+}
+
+export default App;
