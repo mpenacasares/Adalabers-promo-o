10d2d56fc5b2de0063a302edddb12ae07412734d
mpenacasares <m.penacasares@gmail.com>
2022-01-18 10:38:07 +0100

    add newAdalaber function

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,54 @@
 import "../styles/App.scss";
 import contactList from "../data/contacts.json";
+import { useState } from "react";
 
 function App() {
-  const htmlContact = contactList.map((contact) => (
-    <tr key={contact.id}>
+  //  Variables estado
+  const [data, setData] = useState(contactList);
+  const [name, setName] = useState("");
+  const [counselor, setCounselor] = useState("");
+  const [speciality, setSpeciality] = useState("");
+  const [newAdalaber, setNewAdalaber] = useState({
+    name: "",
+    counselor: "",
+    speciality: "",
+  });
+  //  funciones manejadoras
+  const handleNewAdalaberForm = (ev) => {
+    ev.preventDefault();
+  };
+
+  const handleName = (ev) => {
+    setName(ev.currentTarget.value);
+  };
+  const handleCounselor = (ev) => {
+    setCounselor(ev.currentTarget.value);
+  };
+  const handleSpeciality = (ev) => {
+    setSpeciality(ev.currentTarget.value);
+  };
+  const handleNewAdalaberBtn = (ev) => {
+    ev.preventDefault();
+    const newAdalaber = {
+      name: name,
+      counselor: counselor,
+      speciality: speciality,
+    };
+    setData([...data, newAdalaber]);
+    setName("");
+    setCounselor("");
+    setSpeciality("");
+  };
+
+  // render html
+  const htmlContact = data.map((contact, index) => (
+    <tr key={index}>
       <td className="name">{contact.name}</td>
       <td className="counselor">{contact.counselor}</td>
       <td className="speciality">{contact.speciality}</td>
     </tr>
   ));
-  console.log(htmlContact);
+
   return (
     <div className="page">
       {/* header */}
@@ -28,18 +67,58 @@ function App() {
       </header>
       {/* main */}
       <main className="main">
-        <table>
-          {/* <!-- Fila de cabecera --> */}
-          <thead>
-            <tr>
-              <th>Nombre</th>
-              <th>Tutora</th>
-              <th>Especialidad</th>
-            </tr>
-          </thead>
-          {/* <!-- Fin fila de cabecera --> */}
-          <tbody>{htmlContact}</tbody>
-        </table>
+        <section>
+          <table>
+            {/* <!-- Fila de cabecera --> */}
+            <thead>
+              <tr>
+                <th>Nombre</th>
+                <th>Tutora</th>
+                <th>Especialidad</th>
+              </tr>
+            </thead>
+            {/* <!-- Fin fila de cabecera --> */}
+            <tbody>{htmlContact}</tbody>
+          </table>
+        </section>
+        <section>
+          <h2>Añadir nueva Adalaber</h2>
+
+          <form onSubmit={handleNewAdalaberForm}>
+            <label htmlFor="name">Nombre:</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              value={name}
+              onChange={handleName}
+            />
+
+            <label htmlFor="name">Tutora:</label>
+            <input
+              type="text"
+              name="counselor"
+              id="counselor"
+              value={counselor}
+              onChange={handleCounselor}
+            />
+
+            <label htmlFor="name">Especialidad:</label>
+            <input
+              type="text"
+              name="speciality"
+              id="speciality"
+              value={speciality}
+              onChange={handleSpeciality}
+            />
+
+            <input
+              type="submit"
+              value="Añadir nueva Adalaber"
+              onClick={handleNewAdalaberBtn}
+            />
+          </form>
+        </section>
       </main>
     </div>
   );
